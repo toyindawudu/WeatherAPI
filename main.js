b3232fd4bb556aefaf3e9cff73887986ee33ad56
Toyin Dawudu <toyindawudu@gmail.com>
2021-03-17 22:30:58 +0000

    Checkpoint before creating a branch

--- main.js
+++ main.js
@@ -1,42 +1,32 @@
-// Store API key
+// Store API key within an API object
 const api = {
   apiKey:"69ec377536fefde8dd5ef06d398568cb",
   webUrl: "http://api.openweathermap.org/data/2.5/"
 }
 
-
-const searchCity = document.querySelector('user-input');
-searchCity.addEventListener('click', fetchWeather());
-
+const inputValue = document.querySelector('.user-input .card-search .btn');
+inputValue.addEventListener("click", submitSearch);
 
 // create a function that invokes a search when user presses Enter
 function submitSearch(event){
-  if(event.keyCode === 13){
-    // execute fetchWeather function
-    fetchWeather();
+  if(event.keyCode == 13) {
+    getWeather(inputValue.value);
   }
 }
 
-
 // create a function that fetches the weather data
-function fetchWeather (searchCity){
+function getWeather(city){
   fetch(`${api.webUrl}weather?q=${city}&units=metric&appid=${api.apiKey}`)
   .then(weather => {
     return weather.json();
-  }).then(displayWeather)
+  }).then(displayWeather);
 }
 
 function displayWeather(weather){
-  console.log(weather);
+  let city = document.querySelector(".temperature__location .card-search .btn")
+  cityValue.innerText = `${weather.name}`;
 }
 
-
-
-
-
-// document.getElementById("Btn").addEventListener("click", function () {
+// document.querySelector(".card-search .Btn").addEventListener("click", function(){
 //   weather.search();
 // });
-
-// create your functions
- 
